78511d4db8c888c5a3147e815f491d13ac32074b
Oleg Gnidets <oleg.gnidets@okta.com>
2021-12-08 13:17:02 +0200

    Initial Commit

---
+++ MySQL/MySQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySQLApp.swift
+//  MySQL
+//
+//  Created by Oleg Gnidets on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MySQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
