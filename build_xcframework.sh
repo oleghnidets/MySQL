15594fd31c29b96e4e8af0f9ce99eca720d2cb4a
Oleg Gnidets <oleg.oleksan@gmail.com>
2023-08-04 23:55:30 +0300

    Remove support of tvOS and watchOS

--- build_xcframework.sh
+++ build_xcframework.sh
@@ -5,9 +5,5 @@ xcodebuild -create-xcframework \
  -framework ./archive/iphonesimulator/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
  -framework ./archive/macos/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
  -framework ./archive/catalyst/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
- -framework ./archive/watchos/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
- -framework ./archive/watchsimulator/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
- -framework ./archive/appletvos/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
- -framework ./archive/appletvsimulator/MySQL.xcarchive/Products/Library/Frameworks/MySQL.framework \
  -output ./archive/MySQL.xcframework
 
